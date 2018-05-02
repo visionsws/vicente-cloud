572e8807b726dc7e0290c583f0f0fbcd6a97e713
vicente <vicente.wsen@gmail.com>
2018-05-02 16:19:36 +0800

    first commit

---
+++ src/main/java/com/vicente/vicentecloud/VicenteCloudApplication.java
@@ -0,0 +1,12 @@
+package com.vicente.vicentecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VicenteCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VicenteCloudApplication.class, args);
+	}
+}
---
+++ src/test/java/com/vicente/vicentecloud/VicenteCloudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vicente.vicentecloud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VicenteCloudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
